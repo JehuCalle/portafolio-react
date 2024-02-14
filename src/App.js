3456abade8df6ced53ed8f7c549f804bb6aebcd7
JehuCalle <josiasandres56@gmail.com>
2024-02-14 17:12:45 -0300

    #

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
             <Route path="Pages/PiedraPapelTijeraConCSS" element={<PiedraPapelTijeraConCSS/>} />
           </Route>
         </Routes>
+        <Inicio></Inicio>
       </div>
       <footer className='footer-app'>
         <label>paralelo-footer</label>
