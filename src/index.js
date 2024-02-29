89f3f4bd63106df63517fdb9dd7f5db04fec1be9
JehuCalle <josiasandres56@gmail.com>
2024-02-29 17:52:16 -0300

    Remplazo proyecto 4 - Alertas con MUI

--- src/index.js
+++ src/index.js
@@ -13,14 +13,12 @@ import './index.css';
 import App from './App';
 
 import { BrowserRouter } from "react-router-dom";
-import { CssBaseline } from '@mui/material'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CssBaseline />
       <App />
     </BrowserRouter>
   </React.StrictMode>
