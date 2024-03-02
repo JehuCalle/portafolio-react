c4838f225245d33e8b406cf0e95798c90d8eef5a
JehuCalle <josiasandres56@gmail.com>
2024-03-02 18:42:44 -0300

    Experimentando con alertas

--- src/Pages/DiferentesAnim.jsx
+++ src/Pages/DiferentesAnim.jsx
@@ -1,13 +1,42 @@
 import React, {useState, useEffect} from "react";
 
-import Alert from '@mui/material/Alert';
+import { Alert } from "@mui/material";
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import Fade from '@mui/material/Fade';
+import Slide, { SlideProps } from '@mui/material/Slide';
 
+//ICONOS MUI  ICONOS MUI  ICONOS MUI  ICONOS MUI  ICONOS MUI
+import DoneAllIcon from '@mui/icons-material/DoneAll';
 
 import '../Css/DiferentesAnim.css';
 
+
+
+function SlideTransition(props) {
+  return <Slide {...props} direction="up" />;
+}
+
 function DiferentesAnim(){
 
+  const [state, setState] = useState({
+    open: false,
+    Transition: Fade,
+  });
 
+  const handleClick = (Transition) => () => {
+    setState({
+      open: true,
+      Transition,
+    });
+  };
+
+  const handleClose = () => {
+    setState({
+      ...state,
+      open: false,
+    });
+  };
 
   return(
     <div>
@@ -17,7 +46,27 @@ function DiferentesAnim(){
       <div className="cuerpoAnim col-12">
         <div className="contBtnDifAnim col-3">
           <button className="estiloBtnDifAnim col-5">ejemplo 1</button>
-          <Alert>ALERTA CON MUI</Alert>
+
+
+          <div className="col-5">
+          <Button className="estiloBtnDifAnim d-flex col-12" onClick={handleClick(SlideTransition)}>Boton Desplegable 5s</Button>
+          <Snackbar
+            open={state.open}
+            onClose={handleClose}
+            TransitionComponent={state.Transition}
+            key={state.Transition.name}
+            autoHideDuration={5000}>
+            <Alert
+              variant="filled"
+              severity="warning"
+              icon={<DoneAllIcon color="primary" fontSize="inherit"/>}
+              className="text-info border rounded-4 border-5 border-dark">
+              a
+            </Alert>
+          </Snackbar>
+          </div>
+
+
           <button className="estiloBtnDifAnim col-5">ejemplo 2</button>
           <button className="estiloBtnDifAnim col-5">ejemplo 3</button>
           <button className="estiloBtnDifAnim col-5">ejemplo 4</button>
