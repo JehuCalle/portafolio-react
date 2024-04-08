da28feb3833e1ff72909c42d30df9476e49aa948
JehuCalle <josiasandres56@gmail.com>
2024-04-08 17:03:15 -0400

    Inicio: bg con gradiant y bordes cambiados en proyectos y herramientas y boton gitHub animación de iluminación - Proyecto N°1: Altura del chat solucionada

--- src/Componentes/PiePapTijRnd.jsx
+++ src/Componentes/PiePapTijRnd.jsx
@@ -3,6 +3,7 @@ import React, { useEffect,useState } from "react";
 function PiePapTijRnd(props){
 
   return(
+    /* HACER QUE AL PONER EL CURSOR SOBRE UNA OPCIÓN, LA IMAGEN SE HAGA GRANDE Y PEQUEÑO MIENTRAS EL CURSOS SIGUA SOBRE EL BOTON */
     <button
       id={props.id}
       className={props.classNameBTN}
